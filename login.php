a8b5cdf250b84dd36332b257756687817c794029
Trystar360 <tristanfrederick360@gmail.com>
2018-04-24 13:12:46 -0400

    update

--- login.php
+++ login.php
@@ -44,7 +44,14 @@ session_start();
                         Sign Up
                         <div class="collapsible-body">
                             
-                            <h1>Sign Up!</h1>
+                        <form action="includes/signupPhp.php" method="POST">
+                            <input type="text" name="first" placeholder="First Name">
+                            <input type="text" name="last" placeholder="Last Name">
+                            <input type="text" name="email" placeholder="Email">
+                            <input type="text" name="username" placeholder="Username">
+                            <input type="password" name="pass" placeholder="Password">
+                            <button type="submit" name="submit">Sign Up</button>
+                        </form>
                     
                         </div>
                     </li>
